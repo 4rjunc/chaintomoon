2a43f8a4182818390b877a74f16ddabb11656782
4rjunc <arjunmundamani@gmail.com>
2024-11-16 20:24:20 +0700

    🕶  ️Added NOUNSDAO

--- packages/nextjs/components/scaffold-eth/BlockieAvatar.tsx
+++ packages/nextjs/components/scaffold-eth/BlockieAvatar.tsx
@@ -1,17 +1,20 @@
 "use client";
 
 import { AvatarComponent } from "@rainbow-me/rainbowkit";
-import { blo } from "blo";
+
+// Function to generate a 3-digit random value from an address
+const generateImageId = (address: string): string => {
+  const numericHash = Array.from(address).reduce((sum, char) => sum + char.charCodeAt(0), 0) % 1000;
+  return numericHash.toString().padStart(3, "0");
+};
 
 // Custom Avatar for RainbowKit
-export const BlockieAvatar: AvatarComponent = ({ address, ensImage, size }) => (
-  // Don't want to use nextJS Image here (and adding remote patterns for the URL)
-  // eslint-disable-next-line @next/next/no-img-element
-  <img
-    className="rounded-full"
-    src={ensImage || blo(address as `0x${string}`)}
-    width={size}
-    height={size}
-    alt={`${address} avatar`}
-  />
-);
+export const BlockieAvatar: AvatarComponent = ({ address, ensImage, size }) => {
+  const imageId = generateImageId(address as `0x${string}`);
+  const imageUrl = `https://noun.pics/${imageId}.jpg`;
+
+  return (
+    // Rendering the image with a fallback to `ensImage` if available
+    <img className="rounded-full" src={ensImage || imageUrl} width={size} height={size} alt={`${address}`} />
+  );
+};
